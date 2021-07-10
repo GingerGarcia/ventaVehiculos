954f68fcb8ca187a57ddd73e9c064b0af5db2239
Jose Anchundia <josamoya@espol.edu.ec>
2021-07-09 23:46:20 -0500

    Creación de métodos
    tales como añadir vendedor, almacenar vendedor, iniciar sesión

--- src/ec/edu/espol/procesos/ManejadorVendedor.java
+++ src/ec/edu/espol/procesos/ManejadorVendedor.java
@@ -5,13 +5,21 @@
  */
 package ec.edu.espol.procesos;
 
+import ec.edu.espol.model.actores.HashCode;
 import ec.edu.espol.model.usuarios.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static ventavehiculos.Main.*;
 
 
 /**
  *
  * 
- * @author User
+ * @author Jose Anchundia
  */
 public class ManejadorVendedor {
     
@@ -26,5 +34,53 @@ public class ManejadorVendedor {
                 return v;
         }
         return null;
-    }            
+    }    
+    
+    public static boolean file_anadirVendedor(Vendedor v){
+        Validaciones.exitenciaCarpeta("archivos");
+        Path p = Paths.get("archivos/vendedores.txt");
+        boolean existe = Files.exists(p);
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("archivos/vendedores.txt",true))){
+            if (!existe){
+                bw.write("Nombres, apellidos, correo, organizacion, clave\n");
+            }
+            bw.write(v.getNombres()+", "+v.getApellidos()+", "+v.getCorreo_electrico()+", "+v.getOrganizacion()+", "+HashCode.toHexString(HashCode.getSHA(v.getClave()))+"\n");
+            v.setClave(HashCode.toHexString(HashCode.getSHA(v.getClave())));
+            almacenarVendedor(v);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+        return true;
+    }
+    
+     /**
+     *
+     * @param v
+     */
+    public static void almacenarVendedor(Vendedor v){
+       vendedores.add(v);
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public static Vendedor inicioSesionVendedor(){
+        boolean valido;
+        System.out.println(ANSI_BLUE+"\t-Iniciar sesion-"+ANSI_RESET);
+        Correo correo = Validaciones.validarCorreo("Correo electronico: ");
+        String contra = Validaciones.validarEntrada("Contraseña: ", 1);
+        Vendedor v = buscarVendedor(correo);
+        if(v!=null){
+            valido = v.getClave().equals(HashCode.toHexString(HashCode.getSHA(contra)));            
+            if(valido){
+                System.out.println(ANSI_GREEN+"**Inicio de sesion existoso**"+ANSI_RESET);
+                System.out.println("\nBienvenido "+v.getNombres());
+                return v;
+            }                
+        }        
+        System.out.println(ANSI_RED+"Usuario y/o contraseña incorrecto(s)"+ANSI_RESET);
+        return null;
+    }
 }

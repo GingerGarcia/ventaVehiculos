385cccb9e947bf3d18dc358c59259e4367808e18
Jose Murillo <joismuri@espol.edu.ec>
2021-07-10 00:26:37 -0500

    menu vendedor

--- src/ec/edu/espol/procesos/ManejadorMain.java
+++ src/ec/edu/espol/procesos/ManejadorMain.java
@@ -5,7 +5,10 @@
  */
 package ec.edu.espol.procesos;
 
+import ec.edu.espol.model.actores.*;
+
 import ec.edu.espol.model.usuarios.*;
+import java.util.Scanner;
 import static ventavehiculos.Main.*;
 
 /**
@@ -76,13 +79,57 @@ public class ManejadorMain {
             menuVendedor();
         }
     }
-        private static void vendedor_Op1(){
-            
-        }
+           private static void vendedor_Op1(){
+        Vendedor vendedor = Vendedor.crearVendedor(new Scanner(System.in)); 
+            if(vendedor!=null){
+                if (ManejadorVendedor.file_anadirVendedor(vendedor))
+                    System.out.println(ANSI_GREEN+"**Vendedor creado exitosamente**"+ANSI_RESET);
+                else
+                    System.out.println(ANSI_RED+"**Ha ocurrido un error, vendedor no creado**"+ANSI_RESET);
+            }
+            else
+                System.out.println(ANSI_RED+"**Vendedor ya existe, no ha sido creado ninguno nuevo**"+ANSI_RESET);
+    }
+    
         private static void vendedor_Op2(){
-            
+            char continuar = 'S';
+             Vendedor vendedor = ManejadorVendedor.inicioSesionVendedor();
+            if (vendedor!=null){
+                TipoVehiculo tp = ManejadorVehiculo.elegirTipoVehiculo();
+                Vehiculo vehiculo;
+            while(continuar!='N'){
+                vehiculo = Vehiculo.crearVehiculo(new Scanner(System.in),tp);                        
+            if (vehiculo!=null){
+                vehiculo.asignarDueno(vendedor);
+                file_anadirVehiculo(vehiculo, vendedor);
+                System.out.println(ANSI_GREEN+"**Vehiculo registrado**"+ANSI_RESET);
+                continuar = 'N';
+            }else{
+                System.out.println(ANSI_RED+"**Placa ya registrada**"+ANSI_RESET);
+                continuar = Validaciones.validarEntrada("¿Desea ingresar otra? [S-N]: ", 1).trim().toUpperCase().charAt(0);
+            }
+        }                                            
         }
-        private static void vendedor_Op3(){
-            
+    }
+    
+    private static void vendedor_Op3(){
+        Vendedor vendedor = ManejadorVendedor.inicioSesionVendedor();
+        if (vendedor!=null){
+            System.out.println("");
+            Vehiculo vehiculo = ManejadorVehiculo.buscarPlaca(Validaciones.validarEntrada("Ingrese la placa: ", 3),vendedor);
+            if (vehiculo == null)
+                System.out.println(ANSI_RED+vendedor.getNombres()+" "+vendedor.getApellidos()+" no tienes vehiculos registrados con esa placa"+ANSI_RESET);
+            else{
+                Oferta oferta = mostrarOfertas_a_Vendedor(vehiculo);
+                if (oferta!=null){
+                    vendedor.vender(oferta);
+                }else
+                    System.out.println(ANSI_RED+"**No ha realizado ninguna venta**"+ANSI_RESET);
+            }
         }
+    }
+        
+    /**
+     *
+     */
 }

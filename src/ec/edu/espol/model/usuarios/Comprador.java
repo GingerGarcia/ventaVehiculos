9bc61f2f32bf1a7c81c64b7c89cc496196dfec7f
Jose Murillo <joismuri@espol.edu.ec>
2021-07-10 00:52:07 -0500

    metodo crearComprador

--- src/ec/edu/espol/model/usuarios/Comprador.java
+++ src/ec/edu/espol/model/usuarios/Comprador.java
@@ -6,7 +6,12 @@
 package ec.edu.espol.model.usuarios;
 
 import ec.edu.espol.model.actores.Oferta;
+import ec.edu.espol.procesos.ManejadorMain;
+import ec.edu.espol.procesos.ManejadorVendedor;
+import ec.edu.espol.procesos.Validaciones;
 import java.util.ArrayList;
+import java.util.Scanner;
+import static ventavehiculos.Main.*;
 
 /**
  *
@@ -52,4 +57,25 @@ public class Comprador extends Usuario{
     public void agregarOferta(Oferta oferta){
         ofertas.add(oferta);    
     }
+    
+    public static Comprador crearComprador(Scanner sc){
+        String nombre = Validaciones.validarCadena("Nombres: ", 2);
+        String apellido = Validaciones.validarCadena("Apellidos: ", 2);
+        String organizacion = Validaciones.validarCadena("Organizacion: ", 2);
+        Correo correo = Validaciones.validarCorreo("Correo electronico: ");
+        boolean continuar = ManejadorMain.buscarUsuario(correo)!=null;
+        while (continuar){
+            System.out.println(ANSI_RED+"Correo ya registado"+ANSI_RESET);
+            System.out.print("¿Desea ingresar otro correo [S(i)-N(o)]? ");
+            char siNo = sc.nextLine().toUpperCase().charAt(0);
+            continuar = siNo=='S';
+            if (continuar){
+                correo = Validaciones.validarCorreo("Correo electronico: ");
+            }            
+            continuar = ManejadorVendedor.buscarVendedor(correo)!=null;
+        }        
+        String clave = Validaciones.validarEntrada("Clave: ",8);
+        Comprador comprador = new Comprador(nombre,apellido,correo,organizacion,clave);
+        return comprador;
     }
+}
